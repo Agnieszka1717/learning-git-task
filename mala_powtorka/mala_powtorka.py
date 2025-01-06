38fc910ddc8b53dde97c81ef4f34da20e8a88ed3
Agnieszka Jablonska <jablonska96@onet.pl>
2025-01-06 15:50:18 +0100

    Add number of products

--- mala_powtorka/mala_powtorka.py
+++ mala_powtorka/mala_powtorka.py
@@ -3,7 +3,10 @@ zakupy = {
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
 print("Lista zakupów " )
+licznik = 0
 for key, value in zakupy.items() :
     for i in range (0, len(value)):
         value[i] = value[i].capitalize()
+        licznik = licznik + 1
     print(f"Idę do {key.capitalize()} i kupuję tam : {", ".join(value)} ")
+    print (f"W sumie kupuję: {licznik} produktów.")
