24ba0a16618b176617fbbff1fc2b16411dd7477e
Agnieszka Jablonska <jablonska96@onet.pl>
2025-01-06 15:46:32 +0100

    Change the first letter

--- mala_powtorka/mala_powtorka.py
+++ mala_powtorka/mala_powtorka.py
@@ -4,4 +4,6 @@ zakupy = {
 }
 print("Lista zakupów " )
 for key, value in zakupy.items() :
+    for i in range (0, len(value)):
+        value[i] = value[i].capitalize()
     print(f"Idę do {key.capitalize()} i kupuję tam : {", ".join(value)} ")
